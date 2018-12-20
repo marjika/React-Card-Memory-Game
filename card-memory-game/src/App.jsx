dcf018a8dca9a7ebd22abe52bbaa0323d82c81fe
marjika <bluegoo5@hotmail.com>
2018-12-20 12:56:41 -0700

    with wins working using callback

--- card-memory-game/src/App.jsx
+++ card-memory-game/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 import Board from './components/Board';
 import Navbar from './components/Navbar';
 
@@ -7,7 +7,7 @@ import initializeDeck from './deck';
 export default function App() {
   const [cards, setCards] = useState([])
   const [flipped, setFlipped] = useState([])
-  const[dimension, setDimension] = useState(400);
+  const [dimension, setDimension] = useState(400);
   const [solved, setSolved] = useState([]);
   const [disabled, setDisabled] = useState(false);
   const [score, setScore] = useState(0);
@@ -28,6 +28,10 @@ export default function App() {
     return () => window.removeEventListener('resize', resizeListener)
   })
 
+  useEffect(() => {
+    checkScore();
+  }, [score]); // Only re-run the effect if count changes
+
   const handleClick = (id) => {
     setDisabled(true);
     if (flipped.length === 0) {
@@ -40,13 +44,23 @@ export default function App() {
       if (isMatch(id)) {
         setSolved([...solved, flipped[0], id]);
         resetCards();
-        setScore(score + 1);
-        if (score>7) {
-          setWins(wins + 1);
-        }
+        updateScore(score, checkScore);
       } else {
         setTimeout(resetCards, 2000)
       }
+    }
+  }
+
+
+  function updateScore(score, callback) {
+    var newScore = score + 1;
+    setScore(score + 1);
+    callback(newScore);
+  }
+
+  const checkScore = (score) => {
+    if (score>7) {
+      setWins(wins + 1);
     }
   }
 
